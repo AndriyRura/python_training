4a360fc981a720a5bd9bf4b910e21d85a5e5668e
ruraa <randri@ukr.net>
2020-09-02 18:54:07 +0300

    new test 4

--- test_test3.py
+++ test_test3.py
@@ -10,32 +10,32 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+
 class TestTest3():
-  def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
-  
-  # def teardown_method(self, method):
-  #   self.driver.quit()
-  
-  def wait_for_window(self, timeout = 20):
-    time.sleep(round(timeout / 1000))
-    wh_now = self.driver.window_handles
-    wh_then = self.vars["window_handles"]
-    if len(wh_now) > len(wh_then):
-      return set(wh_now).difference(set(wh_then)).pop()
-  
-  def test_test3(self):
-    self.driver.get("https://prom.ua/")
-    self.driver.set_window_size(1258, 614)
-    self.driver.find_element(By.NAME, "search_term").click()
-    self.driver.find_element(By.NAME, "search_term").send_keys("крісло карпове")
-    time.sleep(20)
-    self.driver.find_element(By.CSS_SELECTOR, ".ps-search__buttonIcon--2FLNY").click()
-    time.sleep(20)
-    self.vars["window_handles"] = self.driver.window_handles
-    self.driver.find_element(By.CSS_SELECTOR, ".x-gallery-tile:nth-child(2) .x-image-holder__img").click()
-    self.vars["win7443"] = self.wait_for_window(2000)
-    self.driver.switch_to.window(self.vars["win7443"])
-    self.driver.execute_script("window.scrollTo(0,0)")
-  
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+
+    def teardown_method(self, method):
+      self.driver.quit()
+
+    def wait_for_window(self, timeout=20):
+        time.sleep(round(timeout / 1000))
+        wh_now = self.driver.window_handles
+        wh_then = self.vars["window_handles"]
+        if len(wh_now) > len(wh_then):
+            return set(wh_now).difference(set(wh_then)).pop()
+
+    def test_test3(self):
+        self.driver.get("https://prom.ua/")
+        self.driver.set_window_size(1258, 614)
+        self.driver.find_element(By.NAME, "search_term").click()
+        self.driver.find_element(By.NAME, "search_term").send_keys("крісло карпове")
+        time.sleep(20)
+        self.driver.find_element(By.CSS_SELECTOR, ".ps-search__buttonIcon--2FLNY").click()
+        time.sleep(20)
+        self.vars["window_handles"] = self.driver.window_handles
+        self.driver.find_element(By.CSS_SELECTOR, ".x-gallery-tile:nth-child(2) .x-image-holder__img").click()
+        self.vars["win7443"] = self.wait_for_window(2000)
+        self.driver.switch_to.window(self.vars["win7443"])
+        self.driver.execute_script("window.scrollTo(0,0)")
